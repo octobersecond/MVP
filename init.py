145f178de1a93ae46211f531ba7ab040d0a27d4f
octobersecond <imp.apps.flyingsaucer@gmail.com>
2020-10-30 21:28:48 -0400

    Initial Commit

---
+++ init.py
@@ -0,0 +1,41 @@
+import os
+import smtplib
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+contacts = []
+mail_count = 0
+
+# open file and read the contacts mailing address and append them as items to a list.
+with open('resources/mailing_list.txt', 'r') as filehandle:
+    for line in filehandle:
+        currentMail = line[:-1]
+        contacts.append(currentMail)
+# open external html files and write to a local variable
+with open('resources/index.html','rb') as f:
+   file_data = f.read()
+   file_string = file_data.decode(encoding='UTF-8')
+# open external message file, decode it and store it to a variable
+with open('resources/message.txt','rb') as t:
+    message_body = t.read()
+    message = message_body.decode(encoding='UTF-8')
+
+while mail_count < len(contacts):
+    
+    msg = EmailMessage()
+    msg['Subject'] = ''
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = contacts[mail_count]
+    msg.set_content(message)
+    msg.add_alternative(file_string, subtype='html')
+    
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD) 
+        # smtp.send_message(msg)
+        mail_count += 1
+        print ("[" + str(mail_count) + "] Mail has been sent to " + contacts[mail_count] )
+        
+        
+
