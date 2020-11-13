0f0b94cb7f10f0ef9c2251ca1e5b502c9f3e1361
octobersecond <imp.apps.flyingsaucer@gmail.com>
2020-11-13 18:38:02 +0530

    Added Progress bar and tweaked some code

--- init.py
+++ init.py
@@ -1,5 +1,7 @@
 import os
 import smtplib
+import tqdm
+import time
 from email.message import EmailMessage
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
@@ -14,13 +16,15 @@ with open('resources/mailing_list.txt', 'r') as filehandle:
         currentMail = line[:-1]
         contacts.append(currentMail)
 # open external html files and write to a local variable
-#with open('resources/index.html','rb') as f:
-   #file_data = f.read()
-   #file_string = file_data.decode(encoding='UTF-8')
+with open('resources/index.html','rb') as f:
+   file_data = f.read()
+   file_string = file_data.decode(encoding='UTF-8')
+   f.close()
 # open external message file, decode it and store it to a variable
-with open('resources/message.txt','rb') as t:
-    message_body = t.read()
+with open('resources/message.txt','rb') as f:
+    message_body = f.read()
     message = message_body.decode(encoding='UTF-8')
+    f.close()
 
 def mail_final():
     i = mail_count
@@ -33,14 +37,13 @@ def mail_final():
     
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD) 
-        smtp.send_message(msg)
-        print ("[" + str(i) + "] Mail has been sent to " + contacts[i] )
+        #smtp.send_message(msg)
+        tqdm.tqdm.write("[" + str(i) + "] " + contacts[i] )
 
-while mail_count < len(contacts):
+for one in tqdm.trange(len(contacts), desc ="Progress "):
     mail_final()
     mail_count += 1
-    
-
+    time.sleep(0.1)
         
 
 
